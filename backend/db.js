5ef20d71a1ed8ba90fee8d922e3bf029b33e3792
Oliver <oliver@MSI>
2024-06-18 19:46:20 -0700

    finished job app

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    host: "/var/run/postgresql",
+    database: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    host: "/var/run/postgresql",
+    database: getDatabaseUri()
+  });
+}
+
+db.connect();
+
+module.exports = db;
